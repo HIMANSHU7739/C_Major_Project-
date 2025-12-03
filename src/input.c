3316957ba7fa30bdceeb11be78fd1cc74bb7168b
HIMANSHU7739 <himanshu30506@gmail.com>
2025-12-03 11:39:56 +0530

    input.c

--- src/input.c
+++ src/input.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
+// Read string
 void readString(char *buffer, int size) {
     if (fgets(buffer, size, stdin) != NULL) {
         size_t ln = strlen(buffer);
-        if (ln > 0 && buffer[ln-1] == '\n') buffer[ln-1] = '\0';
+        if (ln > 0 && buffer[ln-1] == '\n') 
+            buffer[ln-1] = '\0'; // Remove newline
     } else {
-        buffer[0] = '\0';
+        buffer[0] = '\0'; // Handle error
     }
 }
 
+// Read integer with validation and error 
 int readInt() {
     int x;
+    // Loop until valid integer is entered
     while (scanf("%d", &x) != 1) {
+        // Clear's invalid input from buffer
         int c;
         while ((c = getchar()) != '\n' && c != EOF);
         printf("Please enter a valid integer: ");
@@ -22,9 +27,12 @@ int readInt() {
     return x;
 }
 
+// Read float with validation and error recovery
 float readFloat() {
     float f;
+    // Loop until valid number is entered
     while (scanf("%f", &f) != 1) {
+        // Clear invalid input from buffer
         int c;
         while ((c = getchar()) != '\n' && c != EOF);
         printf("Please enter a valid number: ");
